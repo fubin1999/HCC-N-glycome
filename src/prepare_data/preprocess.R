bfd88dd2a5f40284c327784ea3106bf84ae1b56d
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-05-06 21:21:28 +0800

    (Important) Update preprocess.R
    The whole logic of preprocessing was updated. When plotting boxplots for each sample, some samples clearly have much shorter boxes, with tails truncated. This phenomenon was probably due to the not so robust imputation method (imputing with the Ratio of Detection). This made me consider using a more powerful imputation method, so I turned to KNN. However, considering the mechanism of KNN imputation, it makes more sense to impute on normalized data. So the data must be normalized with missing values. I used median quotient normalization before, which depends on the median for each sample. I don't know about the details of how glycanr package deals with missing values, but I assume that the median of a sample will be affected by missing values, unless the sample has missing values for less than 50% glycans and all missing values were firstly imputed with some value below the median. Based on these thinkings, the new preprocessing workflow is as follows:
    
    1. Filtering glycans: remove glycans with missing values in more than 50% sample.
    2. Filtering samples: remove samples with missing values for more than 50% glycans.
    3. Normalization: first impute with min / 2 for each sample, then normalize by median quotient, finally put those missing values back.
    4. Imputation: Impute with KNN.

--- src/prepare_data/preprocess.R
+++ src/prepare_data/preprocess.R
@@ -21,39 +21,56 @@ convert <- function(comp) {
 converted <- raw_data |> 
   mutate(glycan = convert(glycan))
 
-# 2. Filter samples-----
-# Filter outlier samples based on the number of glycans detected.
-to_delete <- c("D231", "D219", "D243", "D194", "D212")
-filtered_1 <- converted |> 
-  filter(!sample %in% to_delete)
-
-# 3. Filter glycans-----
+# 2. Filter glycans-----
 # Remove glycans with missing values in more than 50% of samples.
-filtered_2 <- filtered_1 |> 
+filtered_1 <- converted |> 
   group_by(glycan) |> 
   mutate(missing_prop = mean(is.na(value))) |> 
   filter(missing_prop < 0.5) |> 
   ungroup() |> 
   select(-missing_prop)
 
+n_distinct(converted$glycan)
+n_distinct(filtered_1$glycan)
 # Before filtering: 72 glycans
 # After filtering: 62 glycans
 
-# 4. Impute glycans-----
-# Impute missing values using the the minimum value of each sample / 2.
-imputed <- filtered_2 |> 
-  group_by(sample) |> 
-  mutate(min_value = min(value, na.rm = TRUE)) |> 
-  ungroup() |> 
-  mutate(value = if_else(is.na(value), min_value / 2, value)) |> 
-  select(-min_value)
+# 3. Filter samples-----
+to_delete_mannual <- c("D231", "D219", "D243", "D194", "D212")  # Outlier samples from manual inspection
+to_delete_missing <- filtered_1 |>   # Outlier samples with too many missing values
+  summarise(missing_prop = mean(is.na(value)), .by = sample) |> 
+  filter(missing_prop >= 0.5) |> 
+  pull(sample)
+to_delete <- c(to_delete_mannual, to_delete_missing)
+filtered_2 <- filtered_1 |> 
+  filter(!sample %in% to_delete)
 
-# 5. Normalize abundance-----
+# 4. Normalize abundance-----
 # Normalize abundance values using median quotient normalization.
-normalized <- imputed |> 
-  rename(gid = sample) |>  # This is required by glycanr::
+normalized <- filtered_2 |> 
+  # Replace missing values with 0
+  # This step is essential for a robust normalization based on median quotient.
+  # If missing values are not replaced, the medians will be based on different sets of samples.
+  # This will result in a biased normalization.
+  mutate(
+    missing = is.na(value),
+    value = replace_na(value, 0)
+  ) |> 
+  rename(gid = sample) |>  # This is required by glycanr
   glycanr::medianquotientnorm() |> 
-  rename(sample = gid)  # Rename back to sample
+  rename(sample = gid) |>   # Rename back to sample
+  mutate(value = if_else(missing, NA, value)) |> 
+  select(-missing)
 
+# 5. Impute glycans-----
+wide_normalized <- normalized |> 
+  pivot_wider(names_from = glycan, values_from = value)
+imputed <- wide_normalized |> 
+  select(-sample) |> 
+  # Impute with KNN
+  VIM::kNN(k = 10) |> as_tibble() |> 
+  select(-ends_with("imp")) |> 
+  mutate(sample = wide_normalized$sample) |> 
+  pivot_longer(-sample, names_to = "glycan", values_to = "value")
 
-write_csv(normalized, snakemake@output[[1]])
+write_csv(imputed, snakemake@output[[1]])
