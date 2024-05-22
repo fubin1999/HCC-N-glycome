cea1959dfc95f036ce980483270868663fc3278a
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-05-22 16:50:32 +0800

    Calculate FCs for all group pairs.

--- src/diff_analysis/fold_change.R
+++ src/diff_analysis/fold_change.R
@@ -3,8 +3,8 @@ source("renv/activate.R")
 library(tidyverse)
 
 # Read data-----
-# glycan_data <- read_csv("results/data/prepared/processed_abundance.csv")
-# groups <- read_csv("results/data/prepared/groups.csv")
+glycan_data <- read_csv("results/data/prepared/processed_abundance.csv")
+groups <- read_csv("results/data/prepared/groups.csv")
 glycan_data <- read_csv(snakemake@input[[1]])
 groups <- read_csv(snakemake@input[[2]])
 
@@ -16,8 +16,11 @@ data <- glycan_data %>%
 FC_result <- data %>%
   summarise(mean_value = mean(value), .by = c(glycan, group)) %>%
   pivot_wider(names_from = group, values_from = mean_value) %>%
-  mutate(CHB_HCC = HCC / CHB, HC_HCC = HCC / HC, LC_HCC = HCC / LC) %>%
-  select(glycan, CHB_HCC, HC_HCC, LC_HCC) %>%
+  mutate(
+    CHB_HCC = HCC / CHB, HC_HCC = HCC / HC, LC_HCC = HCC / LC,
+    HC_LC = LC / HC, CHB_LC = LC / CHB, HC_CHB = CHB / HC
+  ) %>%
+  select(glycan, CHB_HCC, HC_HCC, LC_HCC, HC_LC, CHB_LC, HC_CHB) %>%
   pivot_longer(-glycan, names_to = "comparison", values_to = "FC") %>%
   separate_wider_delim(comparison, delim = "_", names = c("group1", "group2"))
 
