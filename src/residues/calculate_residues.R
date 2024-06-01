e9da65d74f3631eb3ab755ed67b222cdc1d3aae9
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-06-01 16:16:04 +0800

    Change the way of calculate residues.

--- src/residues/calculate_residues.R
+++ src/residues/calculate_residues.R
@@ -13,10 +13,17 @@ residue_table <- mp_table %>%
     residue = as.factor(residue)
   )
 
+total_abundance <- abundance %>%
+  pivot_longer(-sample, names_to = "glycan", values_to = "value") %>%
+  summarise(total_abundance = sum(value), .by = "sample")
+
 result <- abundance %>%
   pivot_longer(-sample, names_to = "glycan", values_to = "value") %>%
   left_join(residue_table, by = "glycan", relationship = "many-to-many") %>%
   summarise(value = sum(value * n), .by = c(sample, residue)) %>%
+  left_join(total_abundance, by = "sample") %>%
+  mutate(value = value / total_abundance) %>%
+  select(-total_abundance) %>%
   pivot_wider(names_from = "residue", values_from = "value")
 
 write_csv(result, snakemake@output[[1]])
