c4227c51ede0c3065c0c39b29fc7fa177c2d973a
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-06-06 16:10:21 +0800

    Update the colors of TCGA gene expression heatmap.

--- src/TCGA/heatmap.R
+++ src/TCGA/heatmap.R
@@ -13,34 +13,64 @@ glyco_genes <- read_csv(snakemake@input[[3]]) %>% pull(gene_name)
 
 expr_mat <- assay(data, "tpm_unstrand")
 gene_info <- rowData(data) %>% as_tibble()
-sample_info <- colData(data) %>% as_tibble()
+sample_info <- colData(data) %>%
+  as_tibble() %>%
+  sjmisc::rec(sample_type, rec = "Primary Tumor=PT;Solid Tissue Normal=TN", suffix = "")
 
 genes_to_show <- dea_result %>%
   filter(gene_name %in% glyco_genes) %>%
-  filter(abs(logFC) > 1, FDR < 0.01) %>%
   pull(gene_name)
 
 gene_info_sub <- gene_info %>%
   filter(gene_name %in% genes_to_show)
 expr_mat_sub <- expr_mat[gene_info_sub %>% pull(gene_id),]
 rownames(expr_mat_sub) <- gene_info_sub %>% pull(gene_name)
-expr_mat_sub <- t(scale(t(log(expr_mat_sub * 1000 + 1))))
 
-col_split <- sample_info %>%
-  sjmisc::rec(sample_type, rec = "Primary Tumor=PT;Solid Tissue Normal=TN", suffix = "") %>%
-  pull(sample_type)
+# Normalize to make TN samples look alike
+z_scores <- expr_mat_sub %>%
+  as.data.frame() %>%
+  rownames_to_column("gene_name") %>%
+  as_tibble() %>%
+  pivot_longer(-gene_name, names_to = "barcode", values_to = "expr") %>%
+  group_by(gene_name) %>%
+  mutate(
+    log_expr = log(expr * 1000 + 1),
+    z_score = (log_expr - mean(log_expr)) / sd(log_expr)
+  ) %>%
+  ungroup()
 
-pdf(snakemake@output[[1]], width = 3, height = 4)
+TN_medians <- z_scores %>%
+  left_join(sample_info %>% select(barcode, sample_type), by = "barcode") %>%
+  filter(sample_type == "TN") %>%
+  summarise(median_z_score = median(z_score), .by = gene_name)
+
+normed <- z_scores %>%
+  left_join(TN_medians, by = "gene_name") %>%
+  mutate(normed = z_score - median_z_score) %>%
+  select(gene_name, barcode, normed)
+
+mat_to_plot <- normed %>%
+  pivot_wider(names_from = barcode, values_from = normed) %>%
+  column_to_rownames("gene_name") %>%
+  as.matrix() %>%
+  .[rownames(expr_mat_sub), colnames(expr_mat_sub)]
+
+col_split <- sample_info$sample_type
+
+pdf(snakemake@output[[1]], width = 4, height = 6)
+col_fun <- colorRamp2(c(-2., 0, 2.), c("green", "black", "red"))
 Heatmap(
-  expr_mat_sub,
+  mat_to_plot,
+  col = col_fun,
   name = "Z-score",
   border = TRUE,
   show_column_names = FALSE,
+  show_row_names = TRUE,
   cluster_columns = FALSE,
-  cluster_rows = FALSE,
+  cluster_rows = TRUE,
   column_split = col_split,
   top_annotation = HeatmapAnnotation(group = anno_block(
-    gp = gpar(fill = c("#7A848D", "HCC" = "#CC5F5A")))
+    gp = gpar(fill = c("#CC5F5A", "#7A848D")))
   ),
 )
 dev.off()
