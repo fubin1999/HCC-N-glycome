e74badc310f4536b4bc8b6311fe25cd00ef8d03d
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-06-06 15:25:45 +0800

    Update the format of the model metrics tables.

--- src/ml/metrics_table.R
+++ src/ml/metrics_table.R
@@ -2,11 +2,13 @@ library(tidyverse)
 library(gt)
 library(webshot2)
 
-metrics_file <- "results/data/ml/model_performance.csv"
+# metrics_file <- "results/data/ml/model_performance.csv"
 metrics_file <- snakemake@input[[1]]
+
 metrics <- read_csv(metrics_file) |> 
   mutate(
-    comparison = factor(comparison, levels = rev(c("Control/HCC", "HC/HCC", "CHB/HCC", "LC/HCC"))),
+    comparison = str_replace(comparison, "/", " / "),
+    comparison = factor(comparison, levels = c("HC / HCC", "CHB / HCC", "LC / HCC", "Control / HCC")),
     metric = case_match(
       metric,
       "accuracy" ~ "Accuracy",
@@ -18,23 +20,36 @@ metrics <- read_csv(metrics_file) |>
     ),
     metric = factor(metric, levels = c("Accuracy", "Sensitivity", "Specificity", "F1 Score", "ROC AUC", "PR AUC"))
   ) |> 
-  pivot_wider(names_from = metric, values_from = score)
+  pivot_wider(names_from = metric, values_from = score) %>%
+  arrange(comparison)
 
-render_table <- function (data, .subtitle) {
+render_table <- function (data, .title) {
+  last_row <- nrow(data)
   gt(data, rowname_col = "comparison") |>
-    tab_header(title = md("**Model Performance**"), subtitle = md(.subtitle)) |>
+    tab_header(title = md(.title)) |>
     fmt_number(columns = 2:7, decimals = 3) |>
-    tab_footnote("ROC AUC: Receiver Operating Characteristic Area Under the Curve") |>
-    tab_footnote("PR AUC: Precision-Recall Area Under the Curve")
+    cols_align(align = "center") %>%
+    tab_footnote(
+      "ROC AUC: Receiver Operating Characteristic Area Under the Curve",
+      locations = cells_column_labels(columns = `ROC AUC`)
+    ) |>
+    tab_footnote(
+      "PR AUC: Precision-Recall Area Under the Curve",
+      locations = cells_column_labels(columns = `PR AUC`)
+    ) %>%
+    tab_style(
+      style = list(cell_text(weight = "bold")),
+      locations = list(cells_body(rows = last_row), cells_stub(rows = last_row))
+    )
 }
 
 complex_table <- metrics %>%
   filter(model == "Fusion") %>%
   select(-model) %>%
-  render_table("HCC Fusion Classifier")
+  render_table("**HCC Fusion** Classifier")
 simple_table <- metrics %>%
   filter(model == "Slim") %>%
   select(-model) %>%
-  render_table("HCC Slim Classifier")
+  render_table("**HCC Slim** Classifier")
 gtsave(complex_table, snakemake@output[[1]])
 gtsave(simple_table, snakemake@output[[2]])
