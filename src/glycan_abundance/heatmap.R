aa2376f56d3bda5d833c5bfcbbfc166db96327fb
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-06-12 10:47:30 +0800

    Remove glycan annotation from heatmap.

--- src/glycan_abundance/heatmap.R
+++ src/glycan_abundance/heatmap.R
@@ -3,15 +3,13 @@ library(ComplexHeatmap)
 library(circlize)
 
 # Read data-----
-# abundance <- read_csv("results/data/prepared/processed_abundance.csv")
-# groups <- read_csv("results/data/prepared/groups.csv")
-# anova_result <- read_csv("results/data/diff_analysis/ancova_for_glycans.csv")
-# mp_table <- read_csv("results/data/derived_traits/meta_properties.csv")
+abundance <- read_csv("results/data/prepared/processed_abundance.csv")
+groups <- read_csv("results/data/prepared/groups.csv")
+anova_result <- read_csv("results/data/glycan_abundance/ancova_for_glycans.csv")
 
 abundance <- read_csv(snakemake@input[["abundance"]])
 groups <- read_csv(snakemake@input[["groups"]])
 anova_result <- read_csv(snakemake@input[["ancova_result"]])
-mp_table <- read_csv(snakemake@input[["mp_table"]])
 
 groups <- groups |> 
   filter(group != "QC")
@@ -38,40 +36,14 @@ set.seed(42)
 mat <- mat[, sample(ncol(mat))]
 
 # Plot heatmap-----
-col <- colorRamp2(c(-2, 0, 2), c("blue", "white", "red"))
+col <- colorRamp2(c(-2, 0, 2), c("#27408B", "white", "#CD0000"))
 
 col_split <- groups |> 
   column_to_rownames("sample")
 col_split <- col_split[colnames(mat), ]
 col_split <- factor(col_split, levels = c("HC", "CHB", "LC", "HCC"))
 
-row_anno_df <- mp_table |> 
-  select(glycan, type, B, nAnt, nF, nS, nG) |> 
-  rename(Type = type, Bisect = B) |> 
-  sjmisc::rec(Type, rec = "high_mannose=high-mannose;else=copy", suffix = "") |> 
-  filter(glycan %in% diff_glycans) |>
-  column_to_rownames("glycan") |> 
-  mutate(
-    nAnt = factor(nAnt, ordered = TRUE),
-    nF = factor(nF, ordered = TRUE),
-    nS = factor(nS, ordered = TRUE),
-    nG = factor(nG, ordered = TRUE)
-  )
-row_anno_df <- row_anno_df[rownames(mat),]
-row_anno <- rowAnnotation(
-  df = row_anno_df,
-  annotation_name_side = "top",
-  col = list(
-    Type = c("complex" = "#FF8C00", "hybrid" = "#FFD700", "high-mannose" = "#228B22"),
-    Bisect = c(`TRUE` = "grey30", `FALSE` = "grey95"),
-    nAnt = c(`0` = "grey95", `1` = "#CDE6F6", `2` = "#99C6E4", `3` = "#4888B1", `4` = "#0A446A"),
-    nF = c(`0` = "grey95", `1` = "#FF8A8A", `2` = "#EF3A3A"),
-    nS = c(`0` = "grey95", `1` = "#D6A7E8", `2` = "#B674D0", `3` = "#8131A1", `4` = "#6D158F"),
-    nG = c(`0` = "grey95", `1` = "#FFDCA4", `2` = "#FFCC79", `3` = "#DA972A", `4` = "#AC7213")
-  )
-)
-
-pdf(snakemake@output[[1]], width = 7, height = 8)
+pdf(snakemake@output[[1]], width = 5, height = 8)
 set.seed(42)
 ht <- Heatmap(
   mat,
@@ -80,7 +52,6 @@ ht <- Heatmap(
   top_annotation = HeatmapAnnotation(group = anno_block(
     gp = gpar(fill = c("HC" = "#7A848D", "CHB" = "#A2AFA6", "LC" = "#FEC37D", "HCC" = "#CC5F5A")))
   ),
-  right_annotation = row_anno,
   border = TRUE,
   show_column_names = FALSE,
   cluster_columns = FALSE,
