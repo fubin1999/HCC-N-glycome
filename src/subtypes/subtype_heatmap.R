2e4ab109f030d896dc3866cebcead2909cf3f024
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-08-28 18:06:14 +0800

    Update subtype heatmap.
    Make the figure smaller.

--- src/subtypes/subtype_heatmap.R
+++ src/subtypes/subtype_heatmap.R
@@ -2,11 +2,11 @@ library(tidyverse)
 library(ComplexHeatmap)
 library(circlize)
 
-# abundance <- read_csv("results/data/subtypes/batched_corrected.csv")
-# clusters <- read_csv("results/data/subtypes/consensus_cluster_result.csv")
-# anova_result <- read_csv("results/data/subtypes/anova.csv")
-# coexp_modules <- read_csv("results/data/glycan_coexpr/glycan_clusters.csv")
-# clinical <- read_csv("results/data/prepared/clinical.csv")
+abundance <- read_csv("results/data/subtypes/batched_corrected.csv")
+clusters <- read_csv("results/data/subtypes/consensus_cluster_result.csv")
+anova_result <- read_csv("results/data/subtypes/anova.csv")
+coexp_modules <- read_csv("results/data/glycan_coexpr/glycan_clusters.csv")
+clinical <- read_csv("results/data/prepared/clinical.csv")
 
 abundance <- read_csv(snakemake@input[["abundance"]])
 clusters <- read_csv(snakemake@input[["clusters"]])
@@ -36,20 +36,21 @@ mat <- data %>%
   as.matrix() %>%
   t()
 
-col_anno_df <- data %>%
+col_anno_vec <- data %>%
   mutate(subtype = str_glue("Subtype{class}"), subtype = as.factor(subtype)) %>%
   select(sample, subtype) %>%
-  left_join(clinical %>% select(sample, sex, age), by = "sample") %>%
   column_to_rownames("sample") %>%
   .[colnames(mat),]
 set.seed(3)
 col_anno <- HeatmapAnnotation(
-  df = col_anno_df,
+  subtype = col_anno_vec,
   col = list(subtype = c(Subtype1 = "#1b9e77", Subtype2 = "#d95f02", Subtype3 = "#7570b3")),
-  annotation_height = unit(0.5, "cm")
+  annotation_height = unit(0.5, "cm"),
+  show_legend = FALSE,
+  show_annotation_name = FALSE
 )
 
-col_split <- as.factor(col_anno_df$subtype)
+col_split <- as.factor(col_anno_vec)
 
 gcm <- tibble(glycan = diff_glycans) %>%
   left_join(coexp_modules, by = "glycan") %>%
@@ -68,7 +69,7 @@ row_anno <- rowAnnotation(
 
 col <- colorRamp2(c(-2, 0, 2), c("#275D87", "white", "#D26F32"))
 
-pdf(snakemake@output[[1]], width = 8, height = 8)
+pdf(snakemake@output[[1]], width = 5, height = 5)
 set.seed(43)
 Heatmap(
   mat,
@@ -76,6 +77,7 @@ Heatmap(
   col = col,
   cluster_columns = FALSE,
   show_column_names = FALSE,
+  show_row_names = FALSE,
   column_split = col_split,
   row_km = 4,
   row_km_repeats = 100,
