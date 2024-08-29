4972ad7023abc3274b30105accdebefab99505e1
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-08-29 21:45:37 +0800

    Update the clinical heatmap.
    Adding TNM stage, ALBI stage and Child-Pugh stage.

--- src/clinical/clinical_heatmap.R
+++ src/clinical/clinical_heatmap.R
@@ -3,21 +3,31 @@ library(ComplexHeatmap)
 
 # clinical <- read_csv("results/data/prepared/clinical.csv")
 # groups <- read_csv("results/data/prepared/groups.csv")
+
 clinical <- read_csv(snakemake@input[[1]])
 groups <- read_csv(snakemake@input[[2]])
 
 data <- groups %>%
   inner_join(clinical, by = "sample") %>%
   mutate(group = factor(group, levels = c("HC", "CHB", "LC", "HCC"))) %>%
-  arrange(group) %>%
+  rename(
+    Group = group,
+    Sex = sex,
+    Age = age,
+    `Child-Pugh` = child_pugh,
+    `TNM Stage` = TNM_stage,
+    `ALBI Stage` = ALBI_stage
+  ) %>%
+  relocate(AAR, .after = TP) %>%
+  arrange(Group, `TNM Stage`) %>%
   column_to_rownames("sample")
 
-pdf(snakemake@output[[1]], width = 12, height = 8)
-set.seed(2)
+pdf(snakemake@output[[1]], width = 14, height = 8)
+set.seed(16)
 ha <- HeatmapAnnotation(
   df = data,
   col = list(
-    group = c("HC" = "#7A848D", "CHB" = "#A2AFA6", "LC" = "#FEC37D", "HCC" = "#CC5F5A")
+    Group = c("HC" = "#7A848D", "CHB" = "#A2AFA6", "LC" = "#FEC37D", "HCC" = "#CC5F5A")
   )
 )
 ht <- Heatmap(
