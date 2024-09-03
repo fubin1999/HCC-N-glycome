130463072d6a1da4c50cc2be26dae91034079e4a
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-09-03 14:24:16 +0800

    Update the probability ridge plot for ALBI model.

--- src/cor_with_liver_function/ALBI_model_prob_ridge_plot.R
+++ src/cor_with_liver_function/ALBI_model_prob_ridge_plot.R
@@ -11,11 +11,15 @@ plot_data <- preds %>%
   left_join(groups, by = c("row_name" = "sample")) %>%
   mutate(group = factor(group, levels = c("HC", "CHB", "LC", "HCC")))
 
-p <- ggplot(plot_data, aes(`prob.II/III`, group, fill = truth, color = truth)) +
+p <- ggplot(plot_data, aes(`prob.II/III`, group, fill = truth, color = truth, point_color = truth)) +
   geom_vline(xintercept = 0.5, linetype = "dashed", color = "black") +
-  geom_density_ridges(alpha = 0.3, scale = .95) +
+  geom_density_ridges(
+    jittered_points = TRUE, scale = .95,
+    rel_min_height = .01, point_shape = "|", point_size = 3,
+    position = position_points_jitter(height = 0)
+  ) +
   labs(x = "Model Probability of ALBI II/III", fill = "ALBI stage") +
-  theme_classic() +
+  theme_ridges(center = TRUE) +
   theme(
     axis.line.y = element_blank(),
     axis.ticks.y = element_blank(),
@@ -23,6 +27,7 @@ p <- ggplot(plot_data, aes(`prob.II/III`, group, fill = truth, color = truth)) +
   ) +
   guides(
     color = "none",
+    point_color = "none",
     fill = guide_legend(
       override.aes = list(
         fill = c("#427296", "#D26F32"),
@@ -31,9 +36,10 @@ p <- ggplot(plot_data, aes(`prob.II/III`, group, fill = truth, color = truth)) +
   ) +
   scale_x_continuous(limits = c(0, 1)) +
   scale_y_discrete(expand = expansion(mult = c(0.1, 0))) +
-  scale_fill_manual(values = c("#427296", "#D26F32")) +
-  scale_color_manual(values = c("#427296", "#D26F32"))
+  scale_fill_manual(values = c("#42729650", "#D26F3250")) +
+  scale_color_manual(values = c("#427296", "#D26F32")) +
+  scale_discrete_manual("point_color", values = c("#427296", "#D26F32"), guide = "none")
 
 # tgutil::ggpreview(p, width = 5.5, height = 4)
 
-ggsave(snakemake@output[[1]], p, width = 5, height = 3)
+ggsave(snakemake@output[[1]], p, width = 5.5, height = 4)
