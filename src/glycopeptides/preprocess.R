bd65c6584a8f011811a937bbf33f5892a743461d
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-12-09 17:20:53 +0800

    Fix a bug in glycopeptides/preprocess.R

--- src/glycopeptides/preprocess.R
+++ src/glycopeptides/preprocess.R
@@ -1,30 +1,30 @@
 library(tidyverse)
 
 
-data <- read_tsv("data/glycoproteome.list") %>%
-  janitor::clean_names()
+# data <- read_tsv("data/glycoproteome.list") %>%
+#   janitor::clean_names()
 
 data <- read_tsv(snakemake@input[[1]]) %>%
   janitor::clean_names()
 
 groups <- tribble(
   ~raw_name, ~ratio, ~group, ~sample,
-  "20241017-LXJ-MIX1", "ratio_127N_126", "HC", "GPS1",
-  "20241017-LXJ-MIX1", "ratio_127C_126", "CHB", "GPS2",
-  "20241017-LXJ-MIX1", "ratio_128N_126", "LC", "GPS3",
-  "20241017-LXJ-MIX1", "ratio_128C_126", "HCC", "GPS4",
-  "20241017-LXJ-MIX1", "ratio_129N_126", "HC", "GPS5",
-  "20241017-LXJ-MIX1", "ratio_129C_126", "CHB", "GPS6",
-  "20241017-LXJ-MIX1", "ratio_130N_126", "LC", "GPS7",
-  "20241017-LXJ-MIX1", "ratio_130C_126", "HCC", "GPS8",
-  "20241017-LXJ-MIX2", "ratio_127N_126", "HC", "GPS9",
-  "20241017-LXJ-MIX2", "ratio_127C_126", "CHB", "GPS10",
-  "20241017-LXJ-MIX2", "ratio_128N_126", "LC", "GPS11",
-  "20241017-LXJ-MIX2", "ratio_128C_126", "HCC", "GPS12",
-  "20241017-LXJ-MIX2", "ratio_129N_126", "HC", "GPS13",
-  "20241017-LXJ-MIX2", "ratio_129C_126", "CHB", "GPS14",
-  "20241017-LXJ-MIX2", "ratio_130N_126", "LC", "GPS15",
-  "20241017-LXJ-MIX2", "ratio_130C_126", "HCC", "GPS16",
+  "20241017-LXJ-MIX1", "ratio_126_130C", "HC", "GPS1",
+  "20241017-LXJ-MIX1", "ratio_127N_130C", "CHB", "GPS2",
+  "20241017-LXJ-MIX1", "ratio_127C_130C", "LC", "GPS3",
+  "20241017-LXJ-MIX1", "ratio_128N_130C", "HCC", "GPS4",
+  "20241017-LXJ-MIX1", "ratio_128C_130C", "HC", "GPS5",
+  "20241017-LXJ-MIX1", "ratio_129N_130C", "CHB", "GPS6",
+  "20241017-LXJ-MIX1", "ratio_129C_130C", "LC", "GPS7",
+  "20241017-LXJ-MIX1", "ratio_130N_130C", "HCC", "GPS8",
+  "20241017-LXJ-MIX2", "ratio_126_130C", "HC", "GPS9",
+  "20241017-LXJ-MIX2", "ratio_127N_130C", "CHB", "GPS10",
+  "20241017-LXJ-MIX2", "ratio_127C_130C", "LC", "GPS11",
+  "20241017-LXJ-MIX2", "ratio_128N_130C", "HCC", "GPS12",
+  "20241017-LXJ-MIX2", "ratio_128C_130C", "HC", "GPS13",
+  "20241017-LXJ-MIX2", "ratio_129N_130C", "CHB", "GPS14",
+  "20241017-LXJ-MIX2", "ratio_129C_130C", "LC", "GPS15",
+  "20241017-LXJ-MIX2", "ratio_130N_130C", "HCC", "GPS16",
 )
 
 prepared <- data %>%
@@ -41,16 +41,16 @@ prepared <- data %>%
     report_ion_131N = report_ion_131_1382,
     report_ion_131C = report_ion_131_1445
   ) %>%
-  filter(report_ion_126 != 0) %>%
+  filter(report_ion_130C != 0) %>%
   mutate(
-    ratio_127N_126 = report_ion_127N / report_ion_126,
-    ratio_127C_126 = report_ion_127C / report_ion_126,
-    ratio_128N_126 = report_ion_128N / report_ion_126,
-    ratio_128C_126 = report_ion_128C / report_ion_126,
-    ratio_129N_126 = report_ion_129N / report_ion_126,
-    ratio_129C_126 = report_ion_129C / report_ion_126,
-    ratio_130N_126 = report_ion_130N / report_ion_126,
-    ratio_130C_126 = report_ion_130C / report_ion_126,
+    ratio_126_130C = report_ion_126 / report_ion_130C,
+    ratio_127N_130C = report_ion_127N / report_ion_130C,
+    ratio_127C_130C = report_ion_127C / report_ion_130C,
+    ratio_128N_130C = report_ion_128N / report_ion_130C,
+    ratio_128C_130C = report_ion_128C / report_ion_130C,
+    ratio_129N_130C = report_ion_129N / report_ion_130C,
+    ratio_129C_130C = report_ion_129C / report_ion_130C,
+    ratio_130N_130C = report_ion_130N / report_ion_130C,
   ) %>%
   mutate(across(starts_with("ratio"), ~ log2(.))) %>%
   mutate(across(starts_with("ratio"), ~ . - median(.))) %>%
